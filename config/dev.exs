57b236242a79545365c4cccb6516b15e3e4f1bb6
Roberto Estrada <restrada@paridin.com>
2020-03-09 23:27:03 -0600

    update dev/test credentials

--- config/dev.exs
+++ config/dev.exs
@@ -1,12 +1,12 @@
 import Config
 
 config :openpay,
-  client_secret: "sk_ecd2d6bcf4db4c75a5f766d15f86899b",
+  client_secret: "sk_da05dc98605d4e1d880582b4ee621e84",
   api_env: :sandbox,
   merchant_id: "mjtkrswiqtxftemz4tgl",
   webhook_auth: [
-    username: "webhook-openpay",
-    password: "this_is_the_password"
+    username: "FSQ82bKTqCTI4hr6w",
+    password: "aBGVWR0eK1s56zYDfoFEsHuaZqaPxzUkRz"
   ],
   authorizer_auth: [
     username: "openpay-auth",
